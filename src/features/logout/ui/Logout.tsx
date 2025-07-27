c0426507b010b777355c5a243130d94bf3840aaa
vladiks <vladvannikov@gmail.com>
2025-07-27 23:02:06 +0400

    feat: add logout functional

--- src/features/logout/ui/Logout.tsx
+++ src/features/logout/ui/Logout.tsx
@@ -1,8 +1,23 @@
 import { Button } from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
 
+import { logout } from '../api';
+import { useNavigate } from 'react-router-dom'
+
 export function Logout() {
+	const navigate = useNavigate()
+
+	async function handleExitClick(e: React.MouseEvent) {
+		e.preventDefault()
+		const data = await logout() 
+
+		if(!data) return
+
+		if(data.status === 201)
+			navigate('/login')
+	}
+
 	return (
-		<Button startIcon={<LogoutIcon />} sx={{color: 'ActiveBorder'}}>Выйти</Button>
+		<Button startIcon={<LogoutIcon />} sx={{color: 'ActiveBorder'}} onClick={handleExitClick}>Выйти</Button>
 	);
 }
