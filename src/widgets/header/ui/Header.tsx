df501689801ca5caf5d6ac04eca7b935b611cb4f
vladiks <vladvannikov@gmail.com>
2025-07-27 22:46:20 +0400

    feat: add dynamic header title

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,15 +1,17 @@
 import { AppBar, Toolbar, Typography, Box  } from '@mui/material'
 import { Logout } from '@/features/logout/ui/Logout'
+import { useLocation } from 'react-router-dom'
 
 
 export function Header() {
-	
+	const location = useLocation()
+	console.log(location.pathname.includes('create'))
 
 	return (
 		<AppBar position='static' sx={{backgroundColor: '#ffffff', color: '#000000'}}>
 			<Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
 				<Typography variant='h4' component='h4'>
-					Header
+					{location.pathname.includes('create') ? 'Создание пользователя' : location.pathname.includes('edit') ? 'Редактирование пользователя' : 'Список пользователей'}
 				</Typography>
 
 				<Box>
